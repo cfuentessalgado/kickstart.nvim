bcba4801485c3139b035f233e8778162d77a12ce
Cristian Fuentes <cfuentess2010@gmail.com>
2023-04-20 13:41:34 -0400

    Gitsigns keybinds

--- init.lua
+++ init.lua
@@ -109,6 +109,44 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      on_attach = function(bufnr)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, opts)
+          opts = opts or {}
+          opts.buffer = bufnr
+          vim.keymap.set(mode, l, r, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', function()
+          if vim.wo.diff then return ']c' end
+          vim.schedule(function() gs.next_hunk() end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        map('n', '[c', function()
+          if vim.wo.diff then return '[c' end
+          vim.schedule(function() gs.prev_hunk() end)
+          return '<Ignore>'
+        end, { expr = true })
+
+        -- Actions
+        map({ 'n', 'v' }, '<leader>hs', ':Gitsigns stage_hunk<CR>')
+        map({ 'n', 'v' }, '<leader>hr', ':Gitsigns reset_hunk<CR>')
+        map('n', '<leader>hS', gs.stage_buffer, { desc = '[S]tage buffer' })
+        map('n', '<leader>hu', gs.undo_stage_hunk, { desc = '[U]ndo stage hunk' })
+        map('n', '<leader>hR', gs.reset_buffer, { desc = '[R]eset Buffer' })
+        map('n', '<leader>hp', gs.preview_hunk, { desc = '[P]review hunk' })
+        map('n', '<leader>hb', function() gs.blame_line { full = true } end, { desc = '[B]lame Line' })
+        map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = '[T]oggle [B]lame' })
+        map('n', '<leader>hd', gs.diffthis)
+        map('n', '<leader>hD', function() gs.diffthis('~') end)
+        map('n', '<leader>td', gs.toggle_deleted, { desc = '[T]oggle [D]eleted' })
+
+        -- Text object
+        map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>')
+      end
     },
   },
 
